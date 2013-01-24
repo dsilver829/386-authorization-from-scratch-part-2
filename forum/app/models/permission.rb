b3d21685ed8f50d15b710c0d6e531aff656c0359
David Silver <dsilver@candidatemetrics.com>
2013-01-24 15:16:32 -0800

    updating permission

--- forum/app/models/permission.rb
+++ forum/app/models/permission.rb
@@ -1,13 +1,29 @@
 class Permission < Struct.new(:user)
+	def initialize(user)
+		allow :users, [:new, :create]
+		allow	:sessions, [:new, :create, :destroy]
+		allow :topics, [:index, :show]
+		if user
+			allow :users, [:edit, :update]
+			allow :topics, [:new, :create, :edit, :update]
+			allow_all if user.admin?
+		end
+	end
+
   def allow?(controller, action)
-    return true if controller == "sessions"
-    return true if controller == "users" && action.in?(%w[new create])
-    return true if controller == "topics" && action.in?(%w[index show])
-    if user
-      return true if controller == "users" && action.in?(%w[edit update])
-      return true if controller == "topics" && action != "destroy"
-      return true if user.admin?
-    end
-    false
+		@allow_all || @allowed_actions[[controller.to_s, action.to_s]]
   end
+
+	def allow_all
+		@allow_all = true
+	end
+
+	def allow(controllers, actions)
+		@allowed_actions ||= {}
+		Array(controllers).each do |controller|
+			Array(actions).each do |action|
+				@allowed_actions[[controller.to_s, action.to_s]] = true
+			end
+		end
+	end
 end
